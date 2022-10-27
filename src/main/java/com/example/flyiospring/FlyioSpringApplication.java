0e4bbf6c9ddf951b168e3b3bc7eb15cffb267722
WilliamA1pha <s3864077@rmit.edu.vn>
2022-10-28 00:19:35 +0700

    s

---
+++ src/main/java/com/example/flyiospring/FlyioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyioSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flyiospring/FlyioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyioSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
